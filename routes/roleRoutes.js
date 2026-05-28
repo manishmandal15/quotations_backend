aad0654d9367987775139c71f8db520b0d318924
manishmandal15 <141217087+manishmandal15@users.noreply.github.com>
2026-05-28 12:27:28 +0530

    updated

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const RoleController = require("../controllers/RoleController");
+const RoleController = require("../controllers/roleController");
 
 router.get("/", RoleController.getAll);
 router.get("/:id", RoleController.getById);
